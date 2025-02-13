9bfdc12fa6af662c8f0d752db3fed8a2049b0911
Stivensch <gschavez@ucundinamarca.edu.co>
2025-02-13 17:18:17 -0500

    salidas por consola

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,14 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+        System.out.println("hola mundo");
+
+
 
 
     }
